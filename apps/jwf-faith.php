5789e4c607dd457fc6007a10c726e05d2f127be3
Christian Martin <25327135+ct-martin@users.noreply.github.com>
2017-11-09 21:33:38 -0500

    Update jwf-faith.php

--- apps/jwf-faith.php
+++ apps/jwf-faith.php
@@ -5,6 +5,12 @@ $sites = array(
 	'https://en.wikipedia.org/wiki/Free_and_open-source_software',
 	'https://en.wikipedia.org/wiki/Oracle_Corporation#Controversies',
 	'http://ritlug.com',
+	'https://kubernetes.io/',
+	'https://getfedora.org/',
+	'https://musicbrainz.org/',
+	'https://magic.rit.edu/?page_id=1008',
+	'https://blog.justinwflory.com/',
+	'https://copr.fedorainfracloud.org/coprs/jflory7/brightlight/',
 );
 header("Location: " . $sites[rand(0, count($sites) - 1)]);
 ?>
