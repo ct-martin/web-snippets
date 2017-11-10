e1cd07650ef5018ea77a15fd94df5c36251cc096
Christian Martin <25327135+ct-martin@users.noreply.github.com>
2017-11-09 21:49:57 -0500

    Update jwf-faith.php

--- apps/jwf-faith.php
+++ apps/jwf-faith.php
@@ -11,6 +11,7 @@ $sites = array(
 	'https://magic.rit.edu/?page_id=1008',
 	'https://blog.justinwflory.com/',
 	'https://copr.fedorainfracloud.org/coprs/jflory7/brightlight/',
+	'https://www.last.fm/user/jflory7',
 );
 header("Location: " . $sites[rand(0, count($sites) - 1)]);
 ?>
