32fe44dd340158bf0d3f8eeea79d8f0dedd5c357
Christian Martin <25327135+ct-martin@users.noreply.github.com>
2017-11-12 01:50:44 -0500

    Update hotswap.js
    Added hot swapping of the URL and page title (the latter is based on the title snippet in title.php)

--- hotswap.js
+++ hotswap.js
@@ -8,6 +8,13 @@ $(document).ready(function() {
 		}).done(
 			function(data, status){
 				$('.main').html(data);
+				var pgTitle = $(event.target).attr('href').substr(1).replace("-", " ").split("/").reverse().join(" - ").replace(/\b[a-z]/g, function(ch) {return ch.toUpperCase();}).trim();
+				if(pgTitle == "") pgTitle = 'Home';
+				window.history.pushState({
+					"html": data,
+					"title": pgTitle
+				},"", $(event.target).attr('href'));
+				document.title = pgTitle + " | YOUR SITE NAME";
 			}
 		).fail(
 			function() {
@@ -15,4 +22,10 @@ $(document).ready(function() {
 			}
 		);
 	});
+	window.onpopstate = function(evt){
+		if(evt.state){
+			$('.main').html(evt.state.html);
+			document.title = evt.state.title + " | YOUR SITE NAME";
+		}
+	};
 });
